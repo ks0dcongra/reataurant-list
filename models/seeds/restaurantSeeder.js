2f590a69df73b925dd1df213cfb8db9b6d372a46
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-17 00:31:34 +0800

    feat : 將restaurant.json 裡的資料當成種子資料，利用require('路徑/restaurant.json') 的方式引入程式。

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
+const restaurantList = require("../../restaurant.json").results
 const mongodb_url = require('../../mongodb_url')
 // mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect(mongodb_url(), { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
-
+console.log(restaurantList)
 // 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
@@ -13,8 +14,14 @@ db.on('error', () => {
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < 10; i++) {
-    Restaurant.create({ name: `Restname-${i}` })
-  }
+  // for (let i = 0; i < 10; i++) {
+    // Restaurant.create({ name: `RESSSSSST-${i}`, name_en: `RUNT-${i}` })
+    Restaurant.create(restaurantList)
+      .then(() => {
+        console.log("restaurantSeeder done!")
+        db.close()
+      })
+      .catch(err => console.log(err))
+  // }
   console.log('done')
 })
