3b98621c823a0343cd77f5a5d8f42ebc3a0ef831
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-17 00:30:40 +0800

    feat: add body-parser and restaurant.json alter origin 引入restaurant API 之方式

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const mongodb_url = require('./mongodb_url')
 const mongoose = require('mongoose') // 載入 mongoose
+const bodyParser = require("body-parser")
+const restaurantSeed = require('./models/restaurant') // 載入 Todo model
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
+//連線資料庫
 mongoose.connect(mongodb_url(), { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 取得資料庫連線狀態
@@ -25,10 +28,15 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
-})
 
+
+app.get('/', (req, res) => {
+  restaurantSeed.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(restaurantSeed => res.render('index', { restaurantSeed})) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+  // res.render('index', { restaurants: restaurantList.results })
+})
 
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
