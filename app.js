cae0f4d2b99c3b2392d22bbe60c541043a4bd908
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-17 05:21:14 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -112,6 +112,16 @@ app.post('/restaurants/:restaurant_id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+
+// 刪除餐廳
+app.post('/restaurants/:restaurant_id/delete', (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`);
 })
